f4bbde1af9c987307b329fc2664189e7ed2c1739
yuji-hirai <holdout0521@yahoo.co.jp>
2022-05-16 23:57:45 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import "./App.css";
+import { NewHomes, NavBar, MarketingFooter } from "./ui-components";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <HomeCardCollectionss />
+      <MarketingFooter />
     </div>
   );
 }
